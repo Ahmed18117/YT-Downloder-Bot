d5adff5dac7b33474e542d9e7dc30dec35a1d029
Yeasin Mollik <yeasin1603@cseku.ac.bd>
2022-05-13 16:02:21 +0600

    made some improvements!

--- main.py
+++ main.py
@@ -91,7 +91,8 @@ def download_video(update: Update, context: CallbackContext):
                                   text="Download complete! Please wait while I send you the video...")
     context.bot.send_chat_action(chat_id=c_id, action=ChatAction.UPLOAD_VIDEO)
     context.bot.send_video(chat_id=c_id, timeout=1000, video=open(streams[stream_id].default_filename, 'rb'))
-    context.bot.delete_message(chat_id=c_id, message_id=m_id)
+    context.bot.edit_message_text(chat_id=c_id, message_id=m_id,
+                                  text="Finished sending!")
     return ConversationHandler.END
 
 
